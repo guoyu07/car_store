4bffa652e05a9a54a328f7ea1d5d9f53cd141537
lmj <lmj25k@hotmail.com>
2015-11-22 16:30:57 +0800

    1.添加首页banner 2. 兼容 一些老版本手机端浏览器

--- app/scripts/store.js
+++ app/scripts/store.js
@@ -1,7 +1,19 @@
 
 define(['flexslider'], function(){
 
-    console.log($.flexslider)
+  // 应用市场主页焦点图
+  $('#banner').flexslider({
+    animation: 'slide',
+    easing: 'ease',
+    directionNav: false,
+    animationSpeed: 300,
+    slideshowSpeed: 5000,
+    slideshow: true, // 是否自动播放
+    init: function() {
+      console.log(111)
+    }
+  });
+
 
     // var Zombie = function(){
     //     // zombie stuff
